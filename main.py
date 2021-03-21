2e0d46974569598b2f64b7f8fc4ec2fd5f5d6f52
Geisterin <geisterin@mail.ru>
2021-03-21 12:34:11 +0200

    Updated test

---
+++ main.py
@@ -0,0 +1,27 @@
+# number = 500
+# number2 = 500.0
+# text_string = '500'
+#
+# print(help(number))
+#
+# number = 500  #Integer
+# number2 = 500.0  # Float
+# text_string = 'Hello zina'  #String
+# boolean_var = True  # Boolean
+# none_type = None  # None type
+# a = 500
+# b = 200
+#
+# print(a+b)
+# print(500 + 200)
+# print(a + 1000 + b)
+
+
+# a = 500
+# b = 200
+# print(a / b)
+# print(type(a / b))
+
+
+sample_string = 'Hello world'
+print(sample_string)
